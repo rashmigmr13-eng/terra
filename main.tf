54aca8c4369fd9569ac831938877ed11b4460809
rashmigmr13-eng <rashmigmr13@gmail.com>
2025-12-05 15:09:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,62 +1,68 @@
 provider "aws" {
-	region = "us-east-1"
+  region = "us-east-1"
 }
 
-#creating vpc
+# Creating VPC
 resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags = {
     Name = "test-project"
   }
 }
 
-resource "aws_subnet" "subnet-1" {
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+# Subnets
+resource "aws_subnet" "subnet_1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
+
   tags = {
     Name = "Subnet-1"
   }
-
 }
-resource "aws_subnet" "subnet-2" {
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
+
   tags = {
     Name = "Subnet-2"
   }
-
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "myigw" {
   vpc_id = aws_vpc.myvpc.id
+
   tags = {
     Name = "MY-IGW"
   }
 }
 
-# creating the route table
-
+# Route Table
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.myvpc.id
+
   tags = {
     Name = "route-table-custom"
   }
 }
 
+# Public Route
 resource "aws_route" "public_route" {
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id         = aws_route_table.public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.myigw.id
+  gateway_id             = aws_internet_gateway.myigw.id
 }
 
-# associating the route table with subnet
+# Route table association
 resource "aws_route_table_association" "public_subnet_association" {
   route_table_id = aws_route_table.public_route_table.id
-  subnet_id = aws_subnet.subnet-1.id
+  subnet_id      = aws_subnet.subnet_1.id
 }
